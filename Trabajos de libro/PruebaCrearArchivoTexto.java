273ae3d4a08f2cecb6b4c9e73261e17b80632d02
SebaDevCom <sebastiangarciahernandez858@gmail.com>
2024-05-15 15:56:01 -0500

    NUevo

---
+++ Trabajos de libro/PruebaCrearArchivoTexto.java
@@ -0,0 +1,9 @@
+public class PruebaCrearArchivoTexto {
+
+    public static void main(String args[]) {
+        CrearArchivoTexto aplicacion = new CrearArchivoTexto();
+        aplicacion.abrirArchivo();
+        aplicacion.agregarRegistros();
+        aplicacion.cerrarArchivo();
+    } 
+} 
