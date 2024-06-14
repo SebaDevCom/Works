e37e451abce02255a9104398d8779c16556da1b8
SebaDevCom <sebastiangarciahernandez858@gmail.com>
2024-06-14 23:32:35 +0100

    New

---
+++ Test/default.bat
@@ -0,0 +1,47 @@
+@echo off
+
+
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+start https://www.youtube.com/watch?v=hbGiNEjqYL0
+
+
+echo El script se cerrará en 10 segundos...
+timeout /t 10 /nobreak >nul
+
+echo Cerrando navegadores abiertos...
+taskkill /IM chrome.exe /F
+taskkill /IM firefox.exe /F
+taskkill /IM msedge.exe /F
+
+exit
