d9d7b21cb82c293dd8febfad8655664141f82b21
SebaDevCom <sebastiangarciahernandez858@gmail.com>
2024-06-15 03:56:35 +0100

    new

--- Test/default.bat
+++ Test/default.bat
@@ -1,47 +1,41 @@
 @echo off
 
+:: Comprobar si se tienen privilegios de administrador
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
 
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
-start https://www.youtube.com/watch?v=hbGiNEjqYL0
+:getPrivileges
+:: Solicitar elevación de permisos de administrador
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+del "%temp%\getadmin.vbs"
+exit /B
 
+:gotPrivileges
+:: El script continúa aquí con privilegios de administrador
 
-echo El script se cerrará en 10 segundos...
-timeout /t 10 /nobreak >nul
+:: URL del archivo que deseas descargar
+set "fileUrl=https://download.scdn.co/SpotifySetup.exe"
 
-echo Cerrando navegadores abiertos...
-taskkill /IM chrome.exe /F
-taskkill /IM firefox.exe /F
-taskkill /IM msedge.exe /F
+:: Ruta donde se guardará temporalmente el archivo descargado
+set "tempFilePath=%TEMP%\ejemplo.exe"
 
-exit
+:: Descarga el archivo desde Internet utilizando PowerShell
+powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('%fileUrl%', '%tempFilePath%')"
+
+:: Verifica si la descarga fue exitosa
+if not exist "%tempFilePath%" (
+    echo Error: No se pudo descargar el archivo.
+    pause
+    exit /b 1
+)
+
+:: Ejecuta el archivo descargado
+echo Ejecutando el archivo descargado...
+start "" "%tempFilePath%"
+
+echo Proceso completado.
+
+pause
+exit /b 0
